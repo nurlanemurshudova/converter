e32525ab59ff557c21a7ad48addf835f2b81bfb7
nurlanemurshudova <m.nurlane.03@gmail.com>
2022-11-22 21:54:08 +0400

    changed

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-const leftButtons = document.querySelector('.box1 button')
-const rightButtons = document.querySelector('.box2 button')
-const btn = document.querySelectorAll('box button');
-const url = 'https://api.exchangerate.host';
-let leftSideBaseCurrency = ''
-function changeCurrency(event, whichSide, currrency) {
-    if (whichSide === 'left') {
-        leftSideBaseCurrency = currrency;
-        [...leftButtons].forEach((button) => {
-            button.classList.remove('activeButton')
-        })
-    }
-    if (whichSide === 'right') {
-        fetch(`${url}base=${leftSideBaseCurrency}&symbols=${currrency}`)
-            .then((res) => res.json())
-            .then((data) => {
-                const v1 = curInput1.value;
-                curInput2.value=v1*data.rates[currrency];
-            })
-        [...rightButtons].forEach((button) => {
-            button.classList.remove('activeButton')
-        })
-    }
-    event.target.style.backgroundColor = '#833AE0';
-    event.target.style.color = 'white';
-    event.target.classList.add('activeButton');
+const curInput1 = document.querySelector("#first-input");
+const curInput2 = document.querySelector("#second-input");
+const leftButtons = document.querySelectorAll(".box1 button");
+const rightButtons = document.querySelectorAll(".box2 button");
 
+const url = "https://api.exchangerate.host/latest?";
+let leftSideBaseCurrency = "";
 
+function changeCurrency(event, whichSide, currency) {
+  if (whichSide === "left") {
+    leftSideBaseCurrency = currency;
+    [...leftButtons].forEach((button) => {
+      button.classList.remove("activeButton");
+    });
+  }
+  if (whichSide === "right") {
+    fetch(`${url}base=${leftSideBaseCurrency}&symbols=${currency}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if(currency=='USD'){
+        curInput2.value = data.rates.USD * curInput1.value;
+        console.log(data,data.rates.USD)
+        }
+        else if(currency=='RUB'){
+            curInput2.value = data.rates.RUB * curInput1.value;
+            console.log(data,data.rates.RUB)
+        }
+        else if(currency=='EUR'){
+            curInput2.value = data.rates.EUR * curInput1.value;
+            console.log(data,data.rates.EUR)
+        }
+        else if(currency=='GBP'){
+            curInput2.value = data.rates.GBP * curInput1.value;
+            console.log(data,data.rates.GBP)
+        }
+      });
+    [...rightButtons].forEach((button) => {
+      button.classList.remove("activeButton");
+    });
+  }
+  event.target.classList.add("activeButton");
 }
-
-
-
-
-
-
-
-
-
-
-
-
