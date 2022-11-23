ec313333c9c18fb6d6bd3c376d58caf7fa159194
nurlanemurshudova <m.nurlane.03@gmail.com>
2022-11-23 08:35:35 +0400

    new

--- app.js
+++ app.js
@@ -4,35 +4,20 @@ const leftButtons = document.querySelectorAll(".box1 button");
 const rightButtons = document.querySelectorAll(".box2 button");
 
 const url = "https://api.exchangerate.host/latest?";
-let leftSideBaseCurrency = "";
+let leftCurrency = '';
 
 function changeCurrency(event, whichSide, currency) {
   if (whichSide === "left") {
-    leftSideBaseCurrency = currency;
+    leftCurrency = currency;
     [...leftButtons].forEach((button) => {
       button.classList.remove("activeButton");
     });
   }
   if (whichSide === "right") {
-    fetch(`${url}base=${leftSideBaseCurrency}&symbols=${currency}`)
+    fetch(`${url}base=${leftCurrency}&symbols=${currency}`)
       .then((res) => res.json())
       .then((data) => {
-        if(currency=='USD'){
-        curInput2.value = data.rates.USD * curInput1.value;
-        console.log(data,data.rates.USD)
-        }
-        else if(currency=='RUB'){
-            curInput2.value = data.rates.RUB * curInput1.value;
-            console.log(data,data.rates.RUB)
-        }
-        else if(currency=='EUR'){
-            curInput2.value = data.rates.EUR * curInput1.value;
-            console.log(data,data.rates.EUR)
-        }
-        else if(currency=='GBP'){
-            curInput2.value = data.rates.GBP * curInput1.value;
-            console.log(data,data.rates.GBP)
-        }
+        curInput2.value = data.rates[currency] * curInput1.value;
       });
     [...rightButtons].forEach((button) => {
       button.classList.remove("activeButton");
